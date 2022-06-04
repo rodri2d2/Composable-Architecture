cb3c373fbd6cb1fa4faa0575350f743d329b6d07
Rodri <rodri2d2@users.noreply.github.com>
2022-06-04 18:35:47 +0200

    Initial Commit
    Create the project

---
+++ ComposableArchitecture/ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by Rodrigo  Candido on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
